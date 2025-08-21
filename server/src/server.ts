4980efda5fa50dba5967ac180a9880de4e1cb869
Izael gomes <izaelgomes127@gmail.com>
2025-08-20 22:13:42 -0300

    chore: prisma and project structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import { router } from './routes';
 
 const app = express();
-
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+app.use(express.json());
+app.use(router);
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
 });
