7656d8a14365af8587ba9b14d19237098641d417
Izael gomes <izaelgomes127@gmail.com>
2025-08-27 01:23:24 -0300

    fix: remove toaster duplicated

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,16 +5,17 @@ import App from './App.tsx';
 import { Provider } from './components/ui/provider.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/react-query.ts';
-import { Toaster } from './components/ui/toaster.tsx';
+import { WebSocketProvider } from './contexts/WebSocketProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider>
-        <BrowserRouter>
-          <Toaster />
-          <App />
-        </BrowserRouter>
+        <WebSocketProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </WebSocketProvider>
       </Provider>
     </QueryClientProvider>
   </StrictMode>
